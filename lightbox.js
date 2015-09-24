0b2c1bd10d9e35401d1726f599aac3f92568dcfa
alexandra estrada <estrada.ae@gmail.com>
2015-09-23 19:51:20 -0700

    lint js

--- lightbox.js
+++ lightbox.js
@@ -1,25 +1,23 @@
 //LIGHTBOX-MVC
 
-//USE MVC STRUCTURE AND OBJECT ORIENTED JAVASCRIPT TO CREATE INSTAGRAM HASHTAG SEARCH 
-//WITH LIGHTBOX VIEW. USER HAS ABILITY TO SEE NEXT IMAGE FROM LIGHTBOX.
+/*USE MVC STRUCTURE AND OBJECT ORIENTED JAVASCRIPT TO CREATE INSTAGRAM HASHTAG SEARCH 
+WITH LIGHTBOX VIEW. USER HAS ABILITY TO SEE NEXT IMAGE FROM LIGHTBOX. */
 
 
 
-//-----------Instantiate a new instance of Jsonp class on 'enter' of hashtag input.------
+//-----------User instantiates a new instance of Jsonp when entering hashtag search.------
+
 	document.getElementById("submitVal").onkeypress = function(e) {
 	    if(e.which === 13 || e.keyCode === 13) {
 	        e.preventDefault();
 	        var hashtag = document.getElementById("submitVal").value;
-
 	        //call new instance of Jsonp and call prototype function.
 	        var request = new Jsonp(hashtag,'https://api.instagram.com/v1/tags/', 'callbackFunction' );
 	        request.apiCall();
 	    }
 	}
 
-
 //----------Jsonp Constructor Class-------------------
-
 
 	var Jsonp = function(hashtag, url, callbackName) {
 		//extract hashtag from input, jsonp callback name, and api url
@@ -34,12 +32,9 @@
 		    script.src = this.url + this.hashtag + '/media/recent?client_id=ba9624f051464cb1a555717ff309d90b&callback=' + this.callback;
 		    document.head.appendChild(script);
 		    document.head.removeChild(script);
-
 	};
 
-
-//-----------jsonp callback function---------------------
-
+//-----------jsonp callback function serving data and initiating MVC---------------------
 	
 	function callbackFunction(data) {
 		//instantiate new hashtag collection and and new hashtag grid view passing callback data 
@@ -47,8 +42,8 @@
 			gridview = new HashtagGridView(hashtags);
 	}
 
-
 //----------Hashtag Model--------------------------------
+
 	var HashtagModel = function(JSON) {
 		this.img = JSON.images.standard_resolution.url;
 		this.caption = JSON.caption.text;
@@ -56,28 +51,31 @@
 		this.profilepic = JSON.user.profile_picture;
 		this.index = JSON.index;
 	}
+
 //-----------Hashtag Collection---------------------------
+
 	var HashtagCollection = function(JSON) {
 		this.resultsArray = [];
 		for (i = 0; i < JSON.data.length; i ++) {
-			JSON.data[i].index = i
-			this.resultsArray.push(new HashtagModel(JSON.data[i]))
-
+			JSON.data[i].index = i;
+			this.resultsArray.push(new HashtagModel(JSON.data[i]));
 		}
 	}
 
 //------------Hashtag Views--------------------------------
+
 	//display hashtag div results in grid layout
 	var HashtagGridView = function(hashtags) {
 		var gridDiv = document.getElementById('gridviewContainer'),
 			hashtags = hashtags.resultsArray;
 		if(gridDiv.hasChildNodes()) {
-			gridDiv.innerHTML = ''
+			gridDiv.innerHTML = '';
 		}
+		griDiv.style.height = '0px';
 		for(i=0; i < hashtags.length; i++) {
 			var hashtagItem= hashtags[i];
 			//call subview to create individual image divs.
-			gridDiv.appendChild(new HashtagView(hashtagItem, hashtags))
+			gridDiv.appendChild(new HashtagView(hashtagItem, hashtags));
 		}
 		return gridDiv;
 	}
@@ -90,7 +88,6 @@
 		img.className = 'hashtagGridImg';
 		img.setAttribute('src', hashtagItem.img);
 		hashtagDiv.appendChild(img);
-
 		//Grid Event Listener----trigger lightbox view
 		img.addEventListener('click' , function(e) {
 			e.preventDefault();
@@ -98,11 +95,10 @@
 			var lightbox = new LightboxView(hashtagItem, hashtags);
 		});
 		return hashtagDiv;
-
-
 	}
 
 //------------Lightbox Views--------------------------------
+
 	//create Lightbox and content container for slide inside lightbox
 	var LightboxView = function(hashtagItem, hashtags) {
 		var currentPosition = hashtags[hashtagItem.index].index,
@@ -111,7 +107,7 @@
 			lightbox.id = 'lightbox';
 			lightbox.innerHTML = '<div id="arrows"><img id="previous" src="images/prev_arrow.png"/><img id="next" src="images/next_arrow.png"/></div><p id="close">X</p>';
 			document.body.appendChild(lightbox);
-		arrowDispalyCheck();
+			arrowDispalyCheck();
 			
 		var lightboxContainer = document.getElementById('lightbox'),
 			lightboxImg = new LightboxImgView(hashtagItem);
@@ -120,7 +116,7 @@
 		//--------EVENT LISTENERS FOR LIGHTBOX--------------
 
 		document.getElementById('previous').addEventListener('click', function() {
-			currentPosition--
+			currentPosition--;
 			lightboxContainer.removeChild(lightboxImg);
 			lightboxImg = new LightboxImgView(hashtags[hashtagItem.index -= 1]);
 			lightboxContainer.appendChild(lightboxImg);
@@ -155,8 +151,7 @@
 		var slideshow = document.createElement('div'),
 			slide = '<div class="slide"><div class="userinfo"><a href="http://www.instagram.com/'+hashtagItem.username+'"><img class="profilepic" src="' + hashtagItem.profilepic +'"/><p class="username">'+hashtagItem.username+'</p></a></div><img src="'+ hashtagItem.img + '"/><p class="caption">'+ hashtagItem.caption +'</p></div>';
 			slideshow.className = 'slideshow';
-			slideshow.innerHTML += slide;
-		
+			slideshow.innerHTML += slide;	
 		return slideshow
 
 	}
